17edc63164b8ec76e9ce7fd37094b2ec4ac90d52
ZuLu0890 <ayodelepraise2000@gmail.com>
2026-07-31 08:34:26 +0000

    build: add commitlint config enforcing conventional commits with project scopes

---
+++ commitlint.config.cjs
@@ -0,0 +1,46 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "security",
+        "deps",
+      ],
+    ],
+    "subject-case": [2, "always", ["sentence-case", "lower-case"]],
+    "body-max-line-length": [1, "always", 100],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "stellar-contracts",
+        "evm-contracts",
+        "bridge",
+        "relayer",
+        "indexer",
+        "api",
+        "sdk",
+        "frontend",
+        "infra",
+        "docs",
+        "ci",
+        "deps",
+        "release",
+      ],
+    ],
+  },
+};
